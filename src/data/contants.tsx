21af789208f57f461786eb805c9dcbe0f9b846c4
Vinicius Mendes <vinicius@itmob.com.br>
2021-08-27 21:41:36 -0300

    refatorando código

--- src/data/contants.tsx
+++ src/data/contants.tsx
@@ -1,6 +1,6 @@
 const URL = `https://api.themoviedb.org/3`;
 const IMAGEURL = `https://image.tmdb.org/t/p`;
-const APIKEY = "8393542f0d97802c12d24480ce1058c4";
+const APIKEY = "";
 const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
 const EVENTS = {
   PosterClick: "PosterClick",
